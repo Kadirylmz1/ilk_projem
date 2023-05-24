ad156e8ca2261f72f87b995a011f80af935d7654
Kadir Yilmaz <128429613+Kadirylmz1@users.noreply.github.com>
2023-05-24 13:08:37 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'İlkel Blog Uygulaması',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Blog Uygulaması"),
+      ),
+      // ignore: avoid_unnecessary_containers
+      body: Container(
+        child: const Center(
+          child: Text("Merhaba"),
+        ),
+      ),
+    );
+  }
+}
